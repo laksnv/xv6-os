c91bb5a99f2eae88d6ec17944e93903e8f5cd232
Lakshmi Narasiman <lakshmi.narasiman@rutgers.edu>
2014-04-08 19:53:17 -0400

    First commit - Multi-threaded sort

---
+++ multi_thread_sort.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define SIZE 10
+
+int main(void)
+{
+    int a[SIZE], i;
+    
+    for(i = 0; i < SIZE; i++)
+        a[i] = i;
+    
+    
+
+ return 0;
+}
