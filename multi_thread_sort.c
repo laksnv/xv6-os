91d083f10c547e8759a7df12944b8907b19e884c
Lakshmi Narasiman <lakshmi.narasiman@rutgers.edu>
2014-04-09 01:54:13 -0400

    Basic multi-threaded bubble sort done with 1 mutex

--- multi_thread_sort.c
+++ multi_thread_sort.c
@@ -1,16 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
+#include <string.h>
 
 #define SIZE 10
 
-int main(void)
-{
-    int a[SIZE], i;
-    
-    for(i = 0; i < SIZE; i++)
-        a[i] = i;
-    
-    
+pthread_t thread[SIZE - 1];
+pthread_mutex_t lock;
 
- return 0;
+
+void *sort(void *index);
+int a[SIZE];
+
+int main()
+{
+    int i;
+
+    for(i = 0; i < 10; i++)
+        a[i] = i;
+        
+    if (pthread_mutex_init(&lock, NULL) != 0)
+    {
+        printf("\n mutex init failed\n");
+        return 1;
+    }
+    
+    for(int j = 0; j < SIZE * SIZE; j++)
+    {
+        for(i = 1; i < SIZE; i++)
+        {
+            int ret = pthread_create(&thread[i - 1], NULL, sort, (void *) &i);
+            if (ret != 0)
+                printf("\ncan't create thread :[%s]", strerror(ret));
+        }
+
+        for(i = 0; i < 10; i++)
+            pthread_join( thread[i], NULL);
+    
+         pthread_mutex_destroy(&lock);
+    }     
+     
+    for(i = 0; i < 10; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+    
+    
+    pthread_exit(NULL);
 }
+
+void *sort(void *index)
+{
+    pthread_mutex_lock(&lock);
+    
+    int right = *((int *) index), left = right - 1;
+
+    if((a[left] < a[right]) && (left >= 0) && (right < SIZE))
+    {
+        int temp = a[left];
+        a[left] = a[right];
+        a[right] = temp;
+    }
+    
+    pthread_mutex_unlock(&lock);
+    pthread_exit(NULL);
+}
+
+
