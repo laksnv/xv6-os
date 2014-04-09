4ee262905f5dc1568eced40dde95e82c3f983ed1
Lakshmi Narasiman <lakshmi.narasiman@rutgers.edu>
2014-04-09 14:05:34 -0400

    Multi-threaded sort done

--- multi_thread_sort.c
+++ multi_thread_sort.c
@@ -4,65 +4,90 @@
 #include <string.h>
 
 #define SIZE 10
+int a[SIZE], count = 0;
 
-pthread_t thread[SIZE - 1];
-pthread_mutex_t lock;
+pthread_t thread[SIZE];
+pthread_mutex_t lock[SIZE + 1];
 
 
-void *sort(void *index);
-int a[SIZE];
+void *sort (void *index);
+void *check (void * temp);
 
 int main()
 {
     int i;
 
-    for(i = 0; i < 10; i++)
+    for(i = 0; i < SIZE; i++)
         a[i] = i;
-        
-    if (pthread_mutex_init(&lock, NULL) != 0)
-    {
-        printf("\n mutex init failed\n");
-        return 1;
+    
+    for(i = 0; i < SIZE; i++)
+    {    
+        if (pthread_mutex_init(&lock[i], NULL) != 0)
+        {
+            printf("\nMutex init failed\n");
+            return 1;
+        }
     }
     
-    for(int j = 0; j < SIZE * SIZE; j++)
+    while(1)
     {
         for(i = 1; i < SIZE; i++)
         {
             int ret = pthread_create(&thread[i - 1], NULL, sort, (void *) &i);
             if (ret != 0)
-                printf("\ncan't create thread :[%s]", strerror(ret));
-        }
-
-        for(i = 0; i < 10; i++)
+                printf("\nCan't create thread :[%s]", strerror(ret));
             pthread_join( thread[i], NULL);
-    
-         pthread_mutex_destroy(&lock);
+        }
+        
+        int ret = pthread_create(&thread[SIZE - 1], NULL, check, NULL);
+        if (ret != 0)
+            printf("\nCan't create thread :[%s]", strerror(ret));
+        pthread_join( thread[SIZE - 1], NULL);
+
+        
+        for(i = 0; i < SIZE - 1; i++)
+            pthread_join( thread[i], NULL);
     }     
-     
-    for(i = 0; i < 10; i++)
-        printf("%d ", a[i]);
-    printf("\n");
-    
-    
-    pthread_exit(NULL);
+         
+    for(i = 0; i < SIZE; i++)
+        pthread_mutex_destroy(&lock[i]);
+        
+    return 0;
 }
 
 void *sort(void *index)
 {
-    pthread_mutex_lock(&lock);
-    
     int right = *((int *) index), left = right - 1;
+    
+    pthread_mutex_lock(&lock[left]);
+    pthread_mutex_lock(&lock[right]);
 
+    count++;
     if((a[left] < a[right]) && (left >= 0) && (right < SIZE))
     {
         int temp = a[left];
         a[left] = a[right];
         a[right] = temp;
     }
+
+    pthread_mutex_unlock(&lock[right]);
+    pthread_mutex_unlock(&lock[left]);
     
-    pthread_mutex_unlock(&lock);
     pthread_exit(NULL);
 }
 
+void *check (void * temp)
+{
+    pthread_mutex_lock(&lock[SIZE]);
+    
+    for(int i = 0; i < SIZE - 1; i++)
+        if(a[i] < a[i + 1])
+            pthread_exit(NULL);
 
+    for(int i = 0; i < 10; i++)
+        printf("%d ", a[i]);
+    printf("\n Sorted; Count = %d \n", count/10);
+    
+    exit(0);
+    pthread_mutex_unlock(&lock[SIZE]);
+}
